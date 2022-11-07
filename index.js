fe75dff945c6626fcd95d088cd5edf87555c02b7
SanjidaSanju7 <sanjidaakter6789@gmail.com>
2022-11-08 01:00:36 +0600

    add data to database

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const app = express();
 const port = process.env.PORT || 5000;
 require('dotenv').config()
@@ -8,6 +10,17 @@ require('dotenv').config()
 
 app.use(cors());
 app.use(express.json());
+
+
+
+const uri = "mongodb+srv://<username>:<password>@cluster0.q5qwa13.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    client.close();
+});
+
 
 app.get('/', (req, res) => {
     res.send('Service review server is running')
