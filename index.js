d0571a1988535d656078dc000bbd76bc6f2a9428
SanjidaSanju7 <sanjidaakter6789@gmail.com>
2022-11-09 19:15:36 +0600

    delete specific review

--- index.js
+++ index.js
@@ -70,6 +70,15 @@ async function run() {
             res.send(result);
         });
 
+        // delete specific review with id
+
+        app.delete('/reviews/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const result = await reviewCollection.deleteOne(query);
+            res.send(result)
+        })
+
 
 
 
